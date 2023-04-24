707f4942457044d75ad8d080f79ef953161d3c26
rureq <jurekpiwko@gmail.com>
2023-04-25 00:59:34 +0200

    basic functions and data structures

--- cpu.c
+++ cpu.c
@@ -1,9 +1,51 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define A_ID 0b111;
+#define B_ID 0b000;
+#define C_ID 0b001;
+#define D_ID 0b101;
+#define E_ID 0b011;
+#define H_ID 0b100;
+#define L_ID 0b101;
+
+struct registers{
+    uint8_t A, B, C, D, E, F, H, L;
+    uint16_t PC, SP; 
+};
+
+typedef struct registers Registers;
+
+union Uregisters{
+    struct regs{
+        uint8_t A, F, B, C, D, E, H, L;
+    } regs;
+    struct dregs{
+        uint16_t AF, BC, DE, HL;
+    } dregs;
+};
 
 
-uint8_t * readROM(char * filename, long int * size){
+
+struct flags{
+    bool Z, N, H, CY;
+};
+
+uint8_t readbyte(uint8_t *gamefile, uint32_t address){
+    return gamefile[address];
+};
+
+void operate(uint8_t opcode, byte Loperand, char Roperand, Registers *regs){
+    if (opcode == 0x01)
+    {
+        
+    }
+}
+
+uint8_t *readROM(char * filename, long int * size){
     FILE* romFile = fopen(filename, "rb");
     uint8_t * pointer = NULL; 
     if (romFile != NULL){
@@ -24,7 +66,7 @@ uint8_t * readROM(char * filename, long int * size){
 
 int main(){
     long int length = 0;
-    uint8_t * rom = readROM("Pokemon.gb", &length);
+    uint8_t *rom = readROM("Pokemon.gb", &length);
        printf("Pokemon ROM:\n");
 //       for (int i = 0; i <length; i++){
 //        printf("0x%x ",rom[i]);
