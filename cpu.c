dff36144288a77d4b60dc9c94d263de28cb58236
rureq <jurekpiwko@gmail.com>
2023-04-25 03:14:20 +0200

    reading ROM and getching ops works

--- cpu.c
+++ cpu.c
@@ -12,12 +12,23 @@
 #define H_ID 0b100;
 #define L_ID 0b101;
 
+#define ROM_OFFSET = 0x0000;
+#define VRAM_OFFSET = 0x8000;
+#define ERAM_OFFSET = 0xA000;
+#define WRAM_OFFSET = 0xC000;
+#define SAT_OFFSET = 0xFE00; //sprite attribute table
+#define IO_REGS_OFFSET = 0xFF00;
+#define HRAM_OFFSET = 0xFF80;
+#define IE_OFFSET = 0xFFFF; //Interrupt Enable register
+
+
+
 union Registers{
     struct regs{
         uint8_t A, F, B, C, D, E, H, L;
     } regs;
     struct dregs{
-        uint16_t AF, BC, DE, HL;
+        uint16_t FA, CB, ED, LH, SP, PC;
     } dregs;
 };
 
@@ -29,11 +40,51 @@ uint8_t readbyte(uint8_t *gamefile, uint32_t address){
     return gamefile[address];
 };
 
-void operate(uint8_t opcode, byte Loperand, char Roperand, union Registers *regs){
+uint16_t read2bytes(uint8_t *gamefile, uint32_t address){
+    uint16_t twobytes;
+    twobytes = (gamefile[address+1] << 8) | gamefile[address]; //is endianness correct?
+    return twobytes;
+}
+
+void writebyte();
+
+uint8_t getop(uint8_t *gamefile, union Registers *regs){
+    return gamefile[regs->dregs.PC++];
+}
+
+
+enum operations{NOP, LD_BC_d16};
+
+union operation{
+    struct op{
+        char* name;
+        uint8_t opcode;
+        uint8_t length;
+        uint8_t cycles;
+        uint8_t flags : 4; 
+    };
+};
+
+void operate(uint8_t opcode, char Loperand, char Roperand, union Registers *regs){
     if (opcode == 0x01)
     {
         
     }
+    return;
+}
+void readROMNEW(char *filename, uint8_t *memory){
+    FILE* romFile = fopen(filename, "rb");
+    if (romFile != NULL){
+        fseek(romFile, 0, SEEK_END);
+        long int length = ftell(romFile);
+        rewind(romFile); 
+        fread(memory,1,length,romFile);
+        fclose(romFile);
+    } 
+    else{
+        printf("Error reading ROM file\n");
+    }
+
 }
 
 uint8_t *readROM(char * filename, long int * size){
@@ -55,7 +106,23 @@ uint8_t *readROM(char * filename, long int * size){
     return pointer; 
 }
 
+uint8_t *initRAM(){
+    uint8_t *RAM = malloc(sizeof(uint8_t)*16000);
+    return RAM;
+};
+uint8_t *initmemory(){
+    uint8_t *mem = malloc(sizeof(uint8_t)*0xFFFF);
+    if(!mem){
+        printf("asfasf");
+    }
+    return mem;
+};
+
 int main(){
+    union Registers registers;
+    uint8_t *mem = initmemory();
+    readROMNEW("Pokemon.gb", mem);
+    
     long int length = 0;
     uint8_t *rom = readROM("Pokemon.gb", &length);
        printf("Pokemon ROM:\n");
@@ -66,14 +133,22 @@ int main(){
 //       }
         int a = 0;
     char title[16];
-        for(int i = 0; i < 16; i++){
-            title[i] = rom[i+0x134];
-            printf("%c",title[i]); 
-        }
+    for(int i = 0; i < 16; i++){
+        title[i] = rom[i+0x134];
+        printf("%c",title[i]); 
+    }
+    printf("\n");
+    registers.dregs.PC = 0x101;
+    uint8_t firstOP = getop(rom, &registers);
+    printf("first op code: %x\n", firstOP);
+
+    uint16_t secOP = read2bytes(rom, 0x102);
+    printf("first read two bytes: %x\n", secOP);
+    
     while(1){
       scanf("%d",&a);
     }
 
-    free(rom);
+    free(mem);
     return 0;
 }
